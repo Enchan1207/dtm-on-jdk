b24d2abc0be4ae104d42aabe2d968ef2b6899517
enchan <enchant0130@gmail.com>
2025-06-28 23:17:55 +0900

    feat: 環境確認コード

--- app/src/main/java/me/enchan/dtm_on_jdk/App.java
+++ app/src/main/java/me/enchan/dtm_on_jdk/App.java
@@ -1,9 +1,18 @@
 package me.enchan.dtm_on_jdk;
 
+import javax.sound.midi.MidiSystem;
+
 public class App {
 
     private void run() throws Exception {
-        System.out.println("hello, java");
+        final var synthesizer = MidiSystem.getSynthesizer();
+
+        final var deviceInfo = synthesizer.getDeviceInfo();
+        final var name = deviceInfo.getName();
+        final var description = deviceInfo.getDescription();
+
+        System.out.println("name       : " + name);
+        System.out.println("description: " + description);
     }
 
     public static void main(String[] args) {
